f11a74cd58f0474fb7f30f6ec6ca6b525373c091
Nopen <abarovskiy02@mail.ru>
2024-03-20 22:27:25 +0300

    Better navbar

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
-import Navbar from 'react-bootstrap/Navbar';
 import Ad from "../pages/Ad";
 import Signup from "../pages/Signup";
 import Main from "../pages/Main";
@@ -11,27 +10,25 @@ const AppRouter = () => {
     return (
         <div>
             <Router>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                    <Link to="/" className="navbar-brand">Главная</Link>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link">Главная</Link>
-                            </li>
+                <nav className="navbar">
+                    <ul className="link-list left-links">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link main-link">Главная</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/ad" className="nav-link">Объявления</Link>
+                        </li>
+                    </ul>
 
-                            <li className="nav-item">
-                                <Link to="/ad" className="nav-link">Объявления</Link>
-                            </li>
+                    <ul className="link-list right-links">
+                        <li className="nav-item">
+                            <Link to="/signin" className="nav-link">Войти</Link>
+                        </li>
 
-                            <li className="nav-item my-class">
-                                <Link to="/signin" className="nav-link">Войти</Link>
-                            </li>
-
-                            <li className="nav-item justify-content-end">
-                                <Link to="/signup" className="nav-link">Зарегистрироваться</Link>
-                            </li>
-                        </ul>
-                    </div>
+                        <li className="nav-item">
+                            <Link to="/signup" className="nav-link">Зарегистрироваться</Link>
+                        </li>
+                    </ul>
                 </nav>
 
                 <main>
