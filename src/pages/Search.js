ff64cfe117ea194aa6d44c3ebb41d8589836de75
Nopen <abarovskiy02@mail.com>
2024-05-26 16:34:46 +0300

    Pictures setup

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
+import OfferListElement from "../components/OfferListElement";
 
 const Search = () => {
+    useEffect(() => {
+        offersFetch();
+    }, []);
+
+    const [adsInfo, setAdsInfo] = useState([])
+
+    const offersFetch = function () {
+        axios.post('https://mai-houses.onrender.com/houses', {
+            token: Cookies.get("auth_token")
+        })
+            .then(function (response) {
+                console.log(response);
+                setAdsInfo(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     return (
         <div>
-            Поиск
+            <br/><br/><br/>
+            <h1>Объявления</h1>
+            {adsInfo.map((offer, index) => (
+                <OfferListElement name={offer.name} address={offer.adress} price={offer.price} photo_links={offer.photo_links} id={offer._id} key={index}/>
+            ))}
         </div>
     );
 };
