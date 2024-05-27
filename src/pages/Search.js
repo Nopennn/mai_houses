6ed78722ee2a85bccfb99911c5c47025363027a3
Nopen <abarovskiy02@mail.com>
2024-05-27 10:54:26 +0300

    Search type buttons

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -14,14 +14,16 @@ const Unauthorised = () => {
 }
 const Search = () => {
     useEffect(() => {
-        offersFetch();
+        offersFetch(searchType);
     }, []);
 
     const [adsInfo, setAdsInfo] = useState([])
+    const [searchType, setSearchType] = useState("housing")
 
-    const offersFetch = function () {
+    const offersFetch = function (type) {
         axios.post('https://mai-houses.onrender.com/houses', {
-            token: Cookies.get("auth_token")
+            token: Cookies.get("auth_token"),
+            type: type
         })
             .then(function (response) {
                 console.log(response);
@@ -36,9 +38,25 @@ const Search = () => {
         <div>
             <br/><br/>
             <h1>Объявления</h1>
+            <button
+                className = "search-housing" onClick={() => setSearchType("housing")}
+                style={{
+                    backgroundColor: searchType == "housing" ? "#FF8959" : "#ffffff",
+                    color: searchType == "housing" ? "#ffffff" : "#FF8959"
+                }}>
+                Поиск жилья
+            </button>
+            <button
+                className = "search-person" onClick={() => setSearchType("person")}
+                style={{
+                    backgroundColor: searchType == "person" ? "#FF8959" : "#ffffff",
+                    color: searchType == "person" ? "#ffffff" : "#FF8959"
+                }}>
+                Поиск соседей
+            </button>
             {Cookies.get("auth_token") ? null : Unauthorised()}
             {adsInfo.slice(0).reverse().map((offer, index) => (
-                <OfferListElement name={offer.name} address={offer.adress} price={offer.price} photo_links={offer.photo_links} id={offer._id} key={index} about={offer.about}/>
+                <OfferListElement name={offer.name} address={offer.adress} price={offer.price} photo_links={offer.photo_links} id={offer._id} key={index} about={offer.about} type={offer.type}/>
             ))}
         </div>
     );
