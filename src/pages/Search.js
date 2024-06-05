b768321a9e763bec5101900899f8c57e445088e4
Nopen <abarovskiy02@mail.com>
2024-06-05 23:52:46 +0300

    Price range slider

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,7 +3,8 @@ import {Link} from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
 import OfferListElement from "../components/OfferListElement";
-
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
 
 const Unauthorised = () => {
     return (
@@ -12,18 +13,52 @@ const Unauthorised = () => {
         </div>
     )
 }
-const Search = () => {
-    useEffect(() => {
-        offersFetch(searchType);
-    }, []);
 
+
+const Search = () => {
+
+
+    const [gender, setGender] = useState("m")
     const [adsInfo, setAdsInfo] = useState([])
     const [searchType, setSearchType] = useState("housing")
+    const [minPrice, setMinPrice] = useState(10000)
+    const [maxPrice, setMaxPrice] = useState(30000)
+    let rerender = 0;
 
-    const offersFetch = function (type) {
+    useEffect(() => {
+        offersFetch(searchType, minPrice, maxPrice, gender);
+    }, [searchType, rerender]);
+
+    const HousingFilters = () => {
+        return (
+            <div className="slider-container">
+                <h3>Цена, ₽:</h3>
+            </div>
+        )
+    }
+    const PersonFilters = () => {
+
+        return (
+            <div className="slider-container">
+                <h3>Пол:</h3>
+                <input type="radio" value={gender} id="male" name="gender"
+                       onChange={event => setGender("m")}/>
+                <label htmlFor="male">Мужской      </label>
+                <input type="radio" value={gender} id="female" name="gender"
+                       onChange={event => setGender("f")}/>
+                <label htmlFor="female">Женский</label>
+                <h3>Бюджет, ₽:</h3>
+            </div>
+        )
+    }
+    const offersFetch = function (type, min, max, gender) {
+        console.log([gender])
         axios.post('https://mai-houses.onrender.com/houses', {
             token: Cookies.get("auth_token"),
-            type: type
+            types: type,
+            min_price: min,
+            max_price: max,
+            genders: [gender]
         })
             .then(function (response) {
                 console.log(response);
@@ -54,6 +89,24 @@ const Search = () => {
                 }}>
                 Поиск соседей
             </button>
+            {searchType=="housing" ? HousingFilters() : PersonFilters()}
+            <div className="slider-container">
+
+                <RangeSlider className = "price-range" min={0} max={100000} step={1000} value ={[minPrice, maxPrice]} onInput = { (value, userInteraction) => {
+                    setMinPrice(value[0])
+                    setMaxPrice(value[1])
+                }} onThumbDragEnd = {()=>{
+                    offersFetch(searchType, minPrice, maxPrice, gender);
+                }}/>
+                <div className="prices">
+                    <input className="price-borders" type="number" value={minPrice} id="min-price" placeholder="От"
+                           onChange={event => setMinPrice(event.target.value)}/>
+                    <input className="price-borders" type="number" value={maxPrice} id="max-price" placeholder="До"
+                           onChange={event => setMaxPrice(event.target.value)}/>
+                </div>
+            </div>
+
+
             {Cookies.get("auth_token") ? null : Unauthorised()}
             {adsInfo.slice(0).reverse().map((offer, index) => (
                 <OfferListElement name={offer.name} address={offer.adress} price={offer.price} photo_links={offer.photo_links} id={offer._id} key={index} about={offer.about} type={offer.type}/>
